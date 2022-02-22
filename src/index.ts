2e01d1adef71e472b9248e64050fdfb93e0af1df
leandrolf <faccinleandro@gmail.com>
2022-02-22 14:00:23 -0300

    desafio 2

--- src/index.ts
+++ src/index.ts
@@ -41,3 +41,49 @@ app.listen(PORT, () => {
 	// R01, R02, R03, R04, R05
 	
 });
+console.log ("desafio 02")
+	
+	const nome1: string = "ubuntu1 silva";
+	const idade1: number = 33; 
+	const peso1: number = 80;
+	const altura1: number = 1.65; 
+	const ehdev1: boolean = true; 
+
+	const nome2: string = "ubuntu2 silva";
+	const idade2: number = 33; 
+	const peso2: number = 80;
+	const altura2: number = 1.65; 
+	const ehdev2: boolean = true; 
+
+	const nome3: string = "ubuntu3 silva";
+	const idade3: number = 33; 
+	const peso3: number = 80;
+	const altura3: number = 1.65; 
+	const ehdev3: boolean = true; 
+
+	function sum({ idade1, idade2, idade3 }: { idade1: number; idade2: number; idade3: any; }): any {
+		idade1 = +idade1;
+		idade2 = +idade2;
+		idade3 = +idade3;
+		return idade1 + idade2 + idade3;
+	  }
+	
+ var arrayNomes = ["ubuntu1 silva", "ubuntu2 silva", "ubunto3 silva" ];
+	  var resultado = arrayNomes.push("ubuntu1");
+	  console.log(arrayNomes);
+	  console.log(resultado);
+
+
+ function calcularIMCdosubuntus (peso: number, altura: number) {
+		altura = altura / 100;
+		return (peso1 / (altura1 * altura1)) + (peso2/ (altura2 * altura2)) + (peso3/ altura3 * altura3) ;
+	}
+
+
+	function devSilva (): void {
+		for(let chave in arrayNomes) { 
+		console. log (chave, ["nome" + "silva"] );
+		
+	
+		}
+	}
